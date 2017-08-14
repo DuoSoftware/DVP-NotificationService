441e716203f141d1ec895ef392157a06ebd5c9a9
sukitha <sukitha@duosoftware.com>
2017-08-14 10:46:26 +0530

    from number decoding added

--- app.js
+++ app.js
@@ -541,9 +541,6 @@ RestServer.post('/DVP/API/:version/NotificationService/Notification/initiate',au
                 res.end("Message is not stored");
             }
 
-
-
-
         }else {
 
             logger.error('No user available in room', err);
@@ -560,7 +557,7 @@ RestServer.post('/DVP/API/:version/NotificationService/Notification/initiate',au
                     }
                 });
             }
-            else {
+            else if(req.body && req.body.isPersist){
                 DBController.PersistenceMessageRecorder(req, function (errSave, resSave) {
 
                     if (errSave) {
@@ -572,6 +569,9 @@ RestServer.post('/DVP/API/:version/NotificationService/Notification/initiate',au
                         res.end("Message saved until related client is online");
                     }
                 });
+            }else{
+
+                console.log("No Message doesnt persists due to no persists requested.......");
             }
 
 
@@ -583,6 +583,7 @@ RestServer.post('/DVP/API/:version/NotificationService/Notification/initiate',au
     var callObject = {};
     //msg = switch_mprintf("agent_found|%q|%q|%q|%q|%q|%q|inbound|%q", h->member_uuid, skill, cid_number, cid_name, calling_number, h->skills, engagement_type);
 
+    message = decodeURIComponent(message);
     console.log("Message is "+message);
     var messageList = message.split('|');
 
