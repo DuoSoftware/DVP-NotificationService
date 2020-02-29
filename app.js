e818d24106d5bd6757c9bcb8b5279f66760306fa
duoccoder <60223788+duoccoder@users.noreply.github.com>
2020-02-29 12:39:39 +0530

    Handler uncaughtException & unhandledRejection

--- app.js
+++ app.js
@@ -65,6 +65,17 @@ var redisSetting =  {
     }
 };
 
+process.on("uncaughtException", function(err) {
+  console.error(err);
+  console.log("[Unhandled Exception] Node Exiting...");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", err => {
+  console.error(err);
+  console.log("[Unhandled Rejection] Node Exiting...");
+  process.exit(1);
+});
 
 
 if(redismode == 'sentinel'){
